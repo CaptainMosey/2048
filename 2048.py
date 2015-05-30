1ee4fbeb77e08d841727e544b503b4c133ce5d17
CaptainMosey <github@milliliter.cc>
2015-05-30 09:47:26 -0700

    Game adds too much, but is close

--- 2048.py
+++ 2048.py
@@ -46,6 +46,9 @@ class GameState(object):
         def move(self,direction):# direction = [-1,0],[1,0],[0,-1],[0,1]
                 newArray=self.currentState
                 score=0
+                nothingChanged=True
+                alreadyChangedList=[]
+                
                 
                 #set direction
                 if direction == "W":
@@ -59,11 +62,13 @@ class GameState(object):
                                                                 newArray[x,y]=2*newArray[x,y+1]
                                                                 newArray[x,y+1]=0
                                                                 score+=newArray[x,y]
+                                                                nothingChanged=False
 
                                         for y in range(0,3):
                                                 if newArray[x,y]==0:
                                                         newArray[x,y]=newArray[x,y+1]
                                                         newArray[x,y+1]=0
+                                                        if newArray[x,y]!=0:nothingChanged=False
                 elif direction == "E":
 
                         for counter in range(3):#do everythig 3 times 
@@ -75,11 +80,13 @@ class GameState(object):
                                                                 newArray[x,y]=2*newArray[x,y-1]
                                                                 newArray[x,y-1]=0
                                                                 score+=newArray[x,y]
+                                                                nothingChanged=False
 
                                         for y in range(3,0,-1):
                                                 if newArray[x,y]==0:
                                                         newArray[x,y]=newArray[x,y-1]
                                                         newArray[x,y-1]=0
+                                                        if newArray[x,y]!=0:nothingChanged=False
 
 
                 elif direction == "N":
@@ -93,11 +100,13 @@ class GameState(object):
                                                                 newArray[y,x]=2*newArray[y+1,x]
                                                                 newArray[y+1,x]=0
                                                                 score+=newArray[y,x]
+                                                                nothingChanged=False
 
                                         for y in range(0,3):
                                                 if newArray[y,x]==0:
                                                         newArray[y,x]=newArray[y+1,x]
                                                         newArray[y+1,x]=0
+                                                        if newArray[y,x]!=0:nothingChanged=False
                                                                          
                 elif direction == "S":
 
@@ -110,26 +119,63 @@ class GameState(object):
                                                                 newArray[y,x]=2*newArray[y-1,x]
                                                                 newArray[y-1,x]=0
                                                                 score+=newArray[y,x]
+                                                                nothingChanged=False
 
                                         for y in range(3,0,-1):
                                                 if newArray[y,x]==0:
                                                         newArray[y,x]=newArray[y-1,x]
                                                         newArray[y-1,x]=0
+                                                        if newArray[y,x]!=0:nothingChanged=False
      
 
-                                          
+
                 self.score+=score
                 self.currentState=newArray
-                print newArray
-                print self.score
+                
+                if nothingChanged:#then nothing moved, not legal
+                        return False
+                return True
+        def addNumber(self):
+                
+                #figure out how many open spaces there are
+ 
+                zeroList=[]
+                for x in range(16):
+                        if self.currentState[x/4,x%4]==0:
+  
+                                zeroList.append([x/4,x%4])
+                if len(zeroList)==0:#then game is over
+                        print"Game Over"
+                        #really end
+                else:
+                        #print zeroList
+                        newPosition=int(random.random()*len(zeroList))
+                        #print "New position", newPosition,zeroList[newPosition]
+        
+                        #decide is adding a 2 or 4
+                        #90% 2
+                        if random.random()<0.9:
+                                newNum=2
+                        else:
+                                newNum=4
+                        #i have no idea why just adding "zeroList[newPosition]" isnt working here!
+                        self.currentState[zeroList[newPosition][0],zeroList[newPosition][1]]=newNum
+                        
+                        print self.currentState
+                        print self.score
+                     
+
 
 
           
 def a():
         a=GameState()
+        
         while True:
                 k=raw_input("move (N,S,E,W)")
-                a.move(k)
+                if a.move(k):
+  
+                        a.addNumber()
 
-
+        
 a()     
